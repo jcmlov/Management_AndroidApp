7a7f86dda8809fb967fc1d2f466bc21faa0e4e71
cheolminju <cmju0104@gmail.com>
2018-04-20 00:30:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jcmcsy/management/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jcmcsy.management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+    }
+}
