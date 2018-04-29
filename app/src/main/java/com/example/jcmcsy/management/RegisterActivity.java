1464ff0a3206d695d7f9b521b22798260f4d73d2
cheolminju <cmju0104@gmail.com>
2018-04-29 23:38:45 +0900

    안드로이드 회원가입

--- app/src/main/java/com/example/jcmcsy/management/RegisterActivity.java
+++ app/src/main/java/com/example/jcmcsy/management/RegisterActivity.java
@@ -1,10 +1,21 @@
 package com.example.jcmcsy.management;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.example.jcmcsy.management.request.RegisterRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -13,11 +24,53 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText idText = (EditText) findViewById(R.id.idText);
-        EditText passwordText = (EditText) findViewById(R.id.passwordText);
-        EditText nameText = (EditText) findViewById(R.id.nameText);
-        EditText ageText = (EditText) findViewById(R.id.ageText);
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final EditText nameText = (EditText) findViewById(R.id.nameText);
+        final EditText ageText = (EditText) findViewById(R.id.ageText);
+        final EditText emailText = (EditText) findViewById(R.id.emailText);
 
         Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+                String userName = nameText.getText().toString();
+                int userAge = Integer.parseInt(ageText.getText().toString());
+                String userEmail = emailText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("회원 등록에 성공했습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create()
+                                        .show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("회원 등록에 실패했습니다.")
+                                        .setNegativeButton("다시 시도", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(userId, userPassword, userName, userAge, userEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
